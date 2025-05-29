d124dfa9edfcc7b6bf256122a27c64edfe3f4735
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-05-29 12:23:21 -0300

    Importing readUsers and adding news functions

--- src/routes/protectedRoutes.js
+++ src/routes/protectedRoutes.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const { readUsers } = require('../models/userModel');
 const authenticateToken = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// Exemplo de rota protegida (requer autenticação)
 router.get('/protected', authenticateToken, (req, res) => {
-    res.status(200).json({ message: `Bem-vindo, ${req.user.username}! Esta é uma rota protegida.` });
+    const users = readUsers();
+    const currentUser = users.find(u => u.id === req.user.id);
+
+    if (!currentUser) {
+        return res.status(404).json({ message: 'Usuário não encontrado.' });
+    }
+
+    res.status(200).json({
+        message: `Bem-vindo, ${req.user.username}! Esta é uma rota protegida.`,
+        yourBalance: currentUser.balance.toFixed(2), // Exibe o saldo do usuário logado
+        yourAccountNumber: currentUser.accountNumber
+    });
+});
+
+// Rota para obter os dados completos do usuário logado
+router.get('/user-data', authenticateToken, (req, res) => {
+    const users = readUsers();
+    const currentUser = users.find(u => u.id === req.user.id);
+
+    if (!currentUser) {
+        return res.status(404).json({ message: 'Usuário não encontrado.' });
+    }
+
+    // Retorna todos os dados do usuário, exceto a senha
+    const { password, ...userData } = currentUser; // Remove a senha do objeto
+    res.status(200).json({ user: userData });
 });
 
 module.exports = router;
